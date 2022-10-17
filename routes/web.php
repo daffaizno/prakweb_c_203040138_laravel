9f5f3dc7c6f838dd3503bf6f7e5bf3ba3db0c56e
Daffaiz No <naufaldaffa@gmail.com>
2022-10-17 20:25:24 +0700

    Menyelesaikan Video Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Daffaiz Naufal",
+        "email" => "email@mail.com",
+        "image" => "DSC00239.JPG"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

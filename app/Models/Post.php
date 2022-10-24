3d6f1a36a824d306a6144456c679b9253f700ffe
Daffaiz No <naufaldaffa@gmail.com>
2022-10-24 11:14:34 +0700

    Menyelesaikan Video Model, Collection & Controller

---
+++ app/Models/Post.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+
+class Post 
+{
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Daffaiz",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus natus, dolor ratione consequatur aspernatur aliquid voluptatibus ipsam blanditiis repudiandae provident non? Sunt, commodi saepe, voluptate ut atque nam ipsa velit voluptatum ipsum quaerat facilis recusandae qui, nulla neque aspernatur perspiciatis pariatur magni. Iste incidunt, veniam ad perferendis fugiat vero molestias similique fugit, repudiandae dicta, delectus aliquid. Hic similique facilis autem doloribus sequi nemo sint libero voluptas illum earum dicta, ad sed sit architecto soluta debitis expedita dignissimos illo praesentium eum et harum quas ullam nulla? Temporibus nihil quo, delectus eius omnis consequuntur hic dolor necessitatibus non ratione sapiente. Eligendi!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Naufal",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus natus, dolor ratione consequatur aspernatur aliquid voluptatibus ipsam blanditiis repudiandae provident non? Sunt, commodi saepe, voluptate ut atque nam ipsa velit voluptatum ipsum quaerat facilis recusandae qui, nulla neque aspernatur perspiciatis pariatur magni. Iste incidunt, veniam ad perferendis fugiat vero molestias similique fugit, repudiandae dicta, delectus aliquid. "
+        ]
+    ];
+
+    public static function all() {
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug) {
+        $posts = static::all();
+        // $new_post = [];
+        // foreach($posts as $p){
+        //     if($p["slug"] === $slug) {
+        //         $post = $p;
+        //     } 
+        // }
+
+        return $posts->firstWhere('slug', $slug);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,55 +30,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Daffaiz",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus natus, dolor ratione consequatur aspernatur aliquid voluptatibus ipsam blanditiis repudiandae provident non? Sunt, commodi saepe, voluptate ut atque nam ipsa velit voluptatum ipsum quaerat facilis recusandae qui, nulla neque aspernatur perspiciatis pariatur magni. Iste incidunt, veniam ad perferendis fugiat vero molestias similique fugit, repudiandae dicta, delectus aliquid. Hic similique facilis autem doloribus sequi nemo sint libero voluptas illum earum dicta, ad sed sit architecto soluta debitis expedita dignissimos illo praesentium eum et harum quas ullam nulla? Temporibus nihil quo, delectus eius omnis consequuntur hic dolor necessitatibus non ratione sapiente. Eligendi!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Naufal",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus natus, dolor ratione consequatur aspernatur aliquid voluptatibus ipsam blanditiis repudiandae provident non? Sunt, commodi saepe, voluptate ut atque nam ipsa velit voluptatum ipsum quaerat facilis recusandae qui, nulla neque aspernatur perspiciatis pariatur magni. Iste incidunt, veniam ad perferendis fugiat vero molestias similique fugit, repudiandae dicta, delectus aliquid. "
-        ],
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-// halaman single posts
-
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Daffaiz",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus natus, dolor ratione consequatur aspernatur aliquid voluptatibus ipsam blanditiis repudiandae provident non? Sunt, commodi saepe, voluptate ut atque nam ipsa velit voluptatum ipsum quaerat facilis recusandae qui, nulla neque aspernatur perspiciatis pariatur magni. Iste incidunt, veniam ad perferendis fugiat vero molestias similique fugit, repudiandae dicta, delectus aliquid. Hic similique facilis autem doloribus sequi nemo sint libero voluptas illum earum dicta, ad sed sit architecto soluta debitis expedita dignissimos illo praesentium eum et harum quas ullam nulla? Temporibus nihil quo, delectus eius omnis consequuntur hic dolor necessitatibus non ratione sapiente. Eligendi!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Naufal",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus natus, dolor ratione consequatur aspernatur aliquid voluptatibus ipsam blanditiis repudiandae provident non? Sunt, commodi saepe, voluptate ut atque nam ipsa velit voluptatum ipsum quaerat facilis recusandae qui, nulla neque aspernatur perspiciatis pariatur magni. Iste incidunt, veniam ad perferendis fugiat vero molestias similique fugit, repudiandae dicta, delectus aliquid. "
-        ],
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post){
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        } 
-    }
-    return view('post',[
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);

c6beefabc404ccc980244c0e57783761be796752
Daffaiz No <naufaldaffa@gmail.com>
2022-11-05 15:34:00 +0700

    User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,5 +13,23 @@ class RegisterController extends Controller
             'title' => 'register',
             'active' => 'register'
         ]);
+    }
+
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required|max:120',
+            'username' => ['required', 'min:2','max:36','unique:users' ],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:6|max:120'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        // $request->session()->flash->with('success', 'Registrationsuccessfull!');
+        
+        return redirect('/login')->with('success', 'Registration was successful!');
     }
 }

56cbd365e314ecb0712f920e5084793ffddada36
Daffaiz No <naufaldaffa@gmail.com>
2022-11-05 19:35:43 +0700

    Dashboard UI

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+    <div class="row my-3">
+        <div class="col-lg-8">
+            <Article>
+
+                <h2>{{ $post->title }}</h2>
+                <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to posts</a>
+                <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <a href="" class="btn btn-danger"><span data-feather="delete"></span> Delete post</a>
+
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
+
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+                
+
+            </Article>
+
+            <a href="/posts"class='d-block mt-2'>Back</a>
+
+        </div>
+    </div>
+</div>  
+
+@endsection
